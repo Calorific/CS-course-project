a7828d1b30bbfe4c033c4e04de18236f86a003fa
Calorific <kknd3390@mail.ru>
2023-12-17 20:31:41 +0700

    feat(Pages): страница расписания

---
+++ CS-course-project/ViewModel/TimetablePageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+using CS_course_project.Commands;
+using CS_course_project.Model.Services.AuthServices;
+using CS_course_project.model.Storage;
+using CS_course_project.Navigation;
+
+namespace CS_course_project.ViewModel; 
+
+public class TimetablePageViewModel : BaseViewModel {
+    public static ICommand LogOutCommand => Command.Create(LogOut);
+    private static async void LogOut(object? sender, EventArgs e) {
+        await AuthService.LogOut();
+        Navigator.Navigate.Execute("Login", null);
+    }
+
+    private string _title = string.Empty;
+
+    public string Title {
+        get => _title;
+        private set {
+            _title = value;
+            Notify();
+        }
+    }
+
+    private async void Init() {
+        var session = await DataManager.LoadSession();
+        Title = "Расписание " + session?.Data;
+    }
+    
+    public TimetablePageViewModel() {
+        Init();
+    }
+}
